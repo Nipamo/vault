04ea7ef37a568f799df0b47fe3a531c0db273848
Nipamo <pascal.moecking@uni-wuppertal.de>
2026-09-03 22:23:48 +0200

    Feature | Add Entry and List Entries

--- console/console.cpp
+++ console/console.cpp
@@ -1,6 +1,8 @@
 #include "console.h"
+#include "entry.h"
 #include "vault.h"
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 
 constexpr auto kCommandPrefix{"vault"};
@@ -24,8 +26,58 @@ void Console::AskForPasswordAndOpenVault() {
   }
 }
 
+void Console::AddEntryCommand() {
+  std::cout << "--- Add Entry ---\n\n";
+
+  std::string service = ReadInputLine("Service: ");
+  std::string username = ReadInputLine("Username: ");
+  std::string password = ReadInputLine("Password: ");
+
+  Entry new_entry = {
+      .service = service, .username = username, .password = password};
+  vault_->AddEntry(new_entry);
+
+  std::cout << "Entry added successfully!\n\n";
+}
+
+void Console::ListEntriesCommand() {
+  const auto& entries = vault_->GetEntries();
+
+  std::cout << "\n--- Vault Entries ---\n\n";
+
+  std::cout << std::left << std::setw(6) << "ID" << std::setw(24) << "Service"
+            << std::setw(28) << "Username"
+            << "Password\n";
+
+  std::cout << std::string(80, '-') << "\n";
+
+  for (const auto& entry : entries) {
+    std::cout << std::left << std::setw(6) << entry.id << std::setw(24)
+              << entry.service << std::setw(28) << entry.username
+              << entry.password << "\n";
+  }
+
+  std::cout << "\n" << entries.size() << " entry(ies).\n\n";
+}
+
+auto Console::ReadInputLine(const std::string& prompt) -> std::string& {
+  std::cout << prompt;
+  static std::string input_line;
+  std::getline(std::cin, input_line);
+  return input_line;
+}
+
 void Console::Run() {
   while (true) {
     AskForPasswordAndOpenVault();
+
+    std::string input_command;
+    getline(std::cin, input_command);
+
+    if (input_command == "add") {
+      AddEntryCommand();
+    } else if (input_command == "list") {
+      ListEntriesCommand();
+    }
   }
 }
--- src/vault.cpp
+++ src/vault.cpp
@@ -10,3 +10,10 @@ auto Vault::IsLocked() const -> bool { return is_locked_; }
 auto Vault::GetMasterPassword() const -> const std::string& {
   return master_password_;
 }
+
+auto Vault::GetEntries() const -> const std::vector<Entry>& { return entries_; }
+
+void Vault::AddEntry(Entry& entry) {
+  entry.id = entries_.size() + 1;
+  entries_.push_back(entry);
+}
